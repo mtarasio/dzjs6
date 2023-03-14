5eab5dedd3fb08d7162fa347b699870ae132df9b
Taras Mykolaichuk <mtarasio@gmail.com>
2023-03-14 10:45:34 +0200

    second commit

--- script.js
+++ script.js
@@ -1,56 +1,27 @@
-   const shoppingList = [
-      {produсtName: "Хліб", amount: 1, purchased: true, price: 25, totalPrice:" "},
-      {produсtName: "Яйця", amount: 10, purchased: true, price: 6, totalPrice:" "},
-      {produсtName: "Молоко", amount: 1, purchased: true, price: 40, totalPrice:" "},
-      {produсtName: "М'ясна вирізка", amount: 1, purchased: false, price: 160, totalPrice:" "}
-   ];
+
+let products = ['Хліб', 'Молоко', 'Яйця', 'Сир', 'Мясо', 'Овочі', 'Фрукти'];
 
 
-   const printShoppingList = () => {
-      let purchasedList = [];
-      let notPurchasedList = [];
-   
-      for (let i = 0; i < shoppingList.length; i++) {
-      let item = shoppingList[i];
-   
-      item.totalPrice = item.amount * item.price;
-   
-      if (item.purchased) {
-         purchasedList.push(item);
-      } else {
-         notPurchasedList.push(item);
-      }
-      }
-   
-      let sortedList = notPurchasedList.concat(purchasedList);
-   
-      console.log("Список покупок:");
-   
-      for (let i = 0; i < sortedList.length; i++) {
-      let item = sortedList[i];
-   
-      console.log(
-         `${item.produсtName} - ${item.amount} шт. за ${item.price} грн. за шт. (${item.totalPrice} грн.) ${
-            item.purchased ? "ПРИДБАНО" : ""}`
-      );
-      }
-   }
-   
-   const purchaseItem = (produсtName) => {
-      for (let i = 0; i < shoppingList.length; i++) {
-      let item = shoppingList[i];
-   
-      if (item.produсtName === produсtName) {
-         item.purchased = true;
-         console.log(`Продукт "${produсtName}" придбано.`);
-         return;
-      }
-      }
-   
-      console.log(`Продукт "${produсtName}" не знайдено у списку.`);
-   }
-   printShoppingList(shoppingList);
-   purchaseItem("М'ясна вирізка");
-   purchaseItem("мед");
-   purchaseItem("Хліб")
-   printShoppingList(shoppingList);
+let boughtList = document.getElementById('boughtList');
+let toBuyList = document.getElementById('toBuyList');
+
+function addProductToList(product, list) {
+  let item = document.createElement('li');
+  item.textContent = product;
+  item.addEventListener('click', function() {
+    moveProductBetweenLists(item, list);
+  });
+  list.appendChild(item);
+}
+
+function moveProductBetweenLists(item, list) {
+  if (list === boughtList) {
+    toBuyList.appendChild(item);
+  } else {
+    boughtList.appendChild(item);
+  }
+}
+
+for (let i = 0; i < products.length; i++) {
+  addProductToList(products[i], toBuyList);
+}
